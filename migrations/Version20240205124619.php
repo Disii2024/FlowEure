60f77fae5a0b688bb64c295cfb0f02afb65645f9
tiioxic <toto27160@hotmail.fr>
2024-02-05 14:41:57 +0100

    font awesome

--- migrations/Version20240205124619.php
+++ migrations/Version20240205124619.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240205123136 extends AbstractMigration
+final class Version20240205124619 extends AbstractMigration
 {
     public function getDescription(): string
     {
