e433c5ccf82cdc04bcd524ec5866dbfadc861188
Kenny GAUDRY <kenny.gaudry@hotmail.fr>
2024-02-05 13:36:11 +0100

    commande

---
+++ migrations/Version20240205123136.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240205123136 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, date_commande DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', reference VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE detail_commande (id INT AUTO_INCREMENT NOT NULL, product_id INT DEFAULT NULL, commande_id INT DEFAULT NULL, user_id INT DEFAULT NULL, qte INT NOT NULL, prix DOUBLE PRECISION NOT NULL, total DOUBLE PRECISION NOT NULL, INDEX IDX_98344FA64584665A (product_id), INDEX IDX_98344FA682EA2E54 (commande_id), INDEX IDX_98344FA6A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE detail_commande ADD CONSTRAINT FK_98344FA64584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE detail_commande ADD CONSTRAINT FK_98344FA682EA2E54 FOREIGN KEY (commande_id) REFERENCES commande (id)');
+        $this->addSql('ALTER TABLE detail_commande ADD CONSTRAINT FK_98344FA6A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE detail_commande DROP FOREIGN KEY FK_98344FA64584665A');
+        $this->addSql('ALTER TABLE detail_commande DROP FOREIGN KEY FK_98344FA682EA2E54');
+        $this->addSql('ALTER TABLE detail_commande DROP FOREIGN KEY FK_98344FA6A76ED395');
+        $this->addSql('DROP TABLE commande');
+        $this->addSql('DROP TABLE detail_commande');
+    }
+}
